204c755d84d99b9bb12f1fa06ddc84596cbbfa90
latiefbachrululum <112526246+latiefbachrululum@users.noreply.github.com>
2022-09-02 14:23:25 +0700

    Add files via upload

---
+++ python4-tkj4.py
@@ -0,0 +1,49 @@
+#  Rumus Persegi sisi * sisi
+
+
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("Luas persegi adalah", luas_p)
+
+# Rumus persegi panjang Panjang * Lebar
+  
+
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+
+print("Luas Persegi Panjang adalah", luas_pp)
+
+
+# Rumus Trapesium 1/2 * (a+b) * tinggi
+print("\nTrapesium")
+a = 6
+b = 10
+t = 8
+luas_t = (a+b) * t / 2
+print("Luas Trapesium adalah", luas_t)
+
+
+# Rumus Segitiga 1/2 * a * t
+print("\nSegitiga")
+a = 40
+t = 20
+luas_s = a * t / 2
+print("Luas Segitiga adalah", luas_s)
+
+
+# Rumus Jajar genjang a * t
+print("\LuasJajar genjang")
+a = 7
+t = 4
+luas_jg = a * t
+print("Luas Jajar genjang adalah", luas_jg)
+
+
+# Rumus Lingkaran 3.14 * r * r
+print("\LuasLingkaran")
+r = 7
+r = 7
+luas_l = 3.14 * r * r
+print("Luas Lingkaran", luas_l)
